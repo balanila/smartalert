9e9bace8da7f54d3fad063589afa121fb86985c2
balanila <balanila@balanila.com>
2022-08-11 15:53:45 +0300

    Fix tlgrm message, modify 7 param for smart

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ if [ -z $TLGRM_CHAT_ID ]; then
 fi
 
 TLGRM_URL="https://api.telegram.org/bot${TLGRM_TOKEN}/sendMessage?chat_id=${TLGRM_CHAT_ID}&text="
-curl "${TLGRM_URL}Telegram%20Alerting%has%20been%20started"
+curl "${TLGRM_URL}Telegram%20Alerting%20has%20been%20started"
 
 #Create ass. array
 declare -A deviceArray=()
@@ -56,14 +56,14 @@ iterateArray() {
 		param=$(awk -v s="$s" '{ print $s }'<<<$SMART_PARAMS)
 
 		echo "Cheking /dev/$device with smart param $param"
-                smartResult=$(smartctl -A -v 1,raw48:54 /dev/$device | sed 's/^[[:space:]]*//' | sed 's/^0//' | sed -n -e "/^$param[[:space:]]/p" | awk '{ print $10 }')
+                smartResult=$(smartctl -A -v 1,raw48:54 -v 7,raw48:54 /dev/$device | sed 's/^[[:space:]]*//' | sed 's/^0//' | sed -n -e "/^$param[[:space:]]/p" | awk '{ print $10 }')
 		echo "smartResult: $smartResult"
 		if [ -z $smartResult  ]; then 
 			echo "Skipping /dev/$device with parameter $param because parameter is invalid or missing"
 			continue 
 		
 		else 
-			paramName=$(smartctl -A -v 1,raw48:54 /dev/$device | sed 's/^[[:space:]]*//' | sed 's/^0//' | sed -n -e "/^$param[[:space:]]/p" | awk '{ print $2 }')
+			paramName=$(smartctl -A -v 1,raw48:54 -v 7,raw48:54 /dev/$device | sed 's/^[[:space:]]*//' | sed 's/^0//' | sed -n -e "/^$param[[:space:]]/p" | awk '{ print $2 }')
                 	if [ $smartResult -gt ${deviceArray[$device,$param]} ]; then
                        	 	echo "smartResult: $smartResult prev: ${deviceArray[$device,$param]}"
                        	 	sendAlert $smartResult ${deviceArray[$device,$param]} $device $paramName
